587b1697c36028b6922b52110dd9a171c88c2d24
James Macfie <james@macfie.co.nz>
2017-03-02 15:34:00 -0800

    Add option to always show hours

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ module.exports = function(n, options) {
 
   var options = options || {};
 
-  var hours = Math.floor(n / 3600),
+  var h = Math.floor(n / 3600);
+  var hours = h > 9 ? h : '0' + h,
     mins = '0' + Math.floor((n % 3600) / 60),
     secs = '0' + Math.floor((n % 60));
 
@@ -11,10 +12,10 @@ module.exports = function(n, options) {
   secs = secs.substr(secs.length - 2);
 
   if(!isNaN(secs)){
-    if (hours){
-      return hours+':'+mins+':'+secs;  
+    if (h || options.hours){
+      return hours+':'+mins+':'+secs;
     } else {
-      return mins+':'+secs;  
+      return mins+':'+secs;
     };
   } else {
     return '00:00';
--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ var hhmmss = require('..');
 
 var fixtures = {
   256: '04:16',
+  '256h': '00:04:16'
 };
 
 describe('hhmmss', function() {
@@ -17,6 +18,11 @@ describe('hhmmss', function() {
   it('should return 04:16 for 256 seconds', function() {
     var str = hhmmss(256);
     assert.equal(str, fixtures[256]);
+  });
+
+	it('should return 00:04:16 for 256 seconds and options.hours is true', function() {
+    var str = hhmmss(256, { hours: true });
+    assert.equal(str, fixtures['256h']);
   });
 
 });
